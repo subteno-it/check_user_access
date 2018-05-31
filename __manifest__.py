630c2effb43743bc7d74d551d7cef8719944baec
Lynda Laceb <lynda.laceb@subteno-it.fr>
2018-05-31 10:37:32 +0200

    [MIG] Migrated to v11.0

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2017 SYLEAM Info Services
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Check User Access',
+    'version': '11.0.1.0.0',
+    'category': 'Custom',
+    'description': """Allows to easily check users' access rights""",
+    'author': 'Subteno IT',
+    'website': 'http://www.subteno-it.fr/',
+    'depends': ['base'],
+    'update_xml': [
+        'wizard/check_user_access.xml',
+    ],
+    'installable': True,
+    'active': False,
+    'license': 'AGPL-3',
+}
--- wizard/check_user_access.py
+++ wizard/check_user_access.py
@@ -1,98 +1,80 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    check_user_access module for OpenERP, Allows to easily check users' access rights
-#    Copyright (C) 2016 SYLEAM Info Services (<http://www.Syleam.fr/>)
-#              Sylvain Garancher <sylvain.garancher@syleam.fr>
-#              Sebastien LANGE <sebastien.lange@syleam.fr>
-#
-#    This file is a part of check_user_access
-#
-#    check_user_access is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    check_user_access is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2018 SYLEAM Info Services
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.osv import osv, fields
-from openerp import SUPERUSER_ID
-from openerp.tools.translate import _
+from odoo import api, exceptions, fields, models, _, SUPERUSER_ID
 
 
-class check_user_access(osv.TransientModel):
+class CheckUserAccess(models.TransientModel):
     _name = 'check.user.access'
     _description = 'User Access Check'
     _rec_name = 'user_id'
 
-    _columns = {
-        'user_id': fields.many2one('res.users', string='User to check', required=True, help='User you want to check accesses'),
-        'check_user_right_ids': fields.one2many('check.user.right', 'check_access_id', string='Rights', help='List of user applied rights'),
-        'check_user_rule_ids': fields.one2many('check.user.rule', 'check_access_id', string='Rules', help='List of user applied rules'),
-        'model_ids': fields.many2many('ir.model', 'ir_model_check_user_rel', 'access_id', 'model_id', string='Models', help='Models to check for access rights and rules'),
-    }
+    user_id = fields.Many2one(
+        comodel_name='res.users', string='User to check',
+        required=True, help='User you want to check accesses.')
+    check_user_right_ids = fields.One2many(
+        comodel_name='check.user.right', inverse_name='check_access_id',
+        string='Rights', help='List of user applied rights.')
+    check_user_rule_ids = fields.One2many(
+        comodel_name='check.user.rule', inverse_name='check_access_id',
+        string='Rules', help='List of user applied rules.')
+    model_ids = fields.Many2many(
+        comodel_name='ir.model', string='Models',
+        help='Models to check for access rights and rules.')
 
-    def default_get(self, cr, uid, fields_list, context=None):
+    @api.model
+    def default_get(self, fields_list):
         """
         Forbid users other than the superuser to launch this wizard
         """
-        if uid != SUPERUSER_ID:
-            raise osv.except_osv(_('Not allowed'), _('Please connect with admin to use this wizard!'))
+        if self.env.uid != SUPERUSER_ID:
+            raise exceptions.UserError(
+                _('Please connect with admin to use this wizard!'))
 
-        return super(check_user_access, self).default_get(cr, uid, fields_list, context=context)
+        return super().default_get(fields_list)
 
-    def compute_check_user_access(self, cr, uid, ids, context=None):
+    def compute_check_user_access(self):
         """
         Populate the access and rules lines
         """
-        if uid != SUPERUSER_ID:
-            raise osv.except_osv(_('Not allowed'), _('Please connect with admin to use this wizard!'))
+        if self.env.uid != SUPERUSER_ID:
+            raise exceptions.UserError(
+                _('Please connect with admin to use this wizard!'))
 
-        ir_model_obj = self.pool.get('ir.model')
-        ir_model_access_obj = self.pool.get('ir.model.access')
-        ir_rule_obj = self.pool.get('ir.rule')
-
-        self.write(cr, uid, ids, {
+        self.write({
             'check_user_right_ids': [(5,)],
-            'check_user_rule_ids': [(5,)]
-        }, context=context)
+            'check_user_rule_ids': [(5,)],
+        })
 
-        for wizard in self.browse(cr, uid, ids, context=context):
-            ir_model_ids = [model.id for model in wizard.model_ids]
-            if not ir_model_ids:
-                ir_model_ids = ir_model_obj.search(cr, uid, [], order='model', context=context)
+        for wizard in self:
+            ir_models = wizard.model_ids
+            if not ir_models:
+                ir_models = self.env['ir.model'].search([], order='model')
 
             modes = ('read', 'write', 'create', 'unlink')
             check_rights = []
             check_rules = []
-            for ir_model in ir_model_obj.browse(cr, uid, ir_model_ids, context=context):
-                checked_model = self.pool.get(ir_model.model)
-
-                # Skip deleted models
-                if not checked_model:
-                    continue
+            for ir_model in ir_models:
+                checked_model = self.env[ir_model.model]
 
                 # Access rights are not checked on TransientModels
-                if isinstance(checked_model, osv.osv_memory):
+                if isinstance(checked_model, models.TransientModel):
                     continue
 
                 rights = {}
                 rules = {}
                 for mode in modes:
-                    rights[mode] = ir_model_access_obj.check(cr, wizard.user_id.id, ir_model.model, mode, False, context)
-                    rules[mode] = ir_rule_obj.domain_get(cr, wizard.user_id.id, ir_model.model, mode=mode, context=context)
+                    rights[mode] = self.env['ir.model.access'].sudo(
+                        user=wizard.user_id).check(
+                            ir_model.model, mode=mode, raise_exception=False)
+                    rules[mode] = self.env['ir.rule'].sudo(
+                        user=wizard.user_id).domain_get(
+                            ir_model.model, mode=mode)
 
                     if rules[mode][0] or rules[mode][1]:
                         where = ' AND '.join(rules[mode][0])
-                        where = cr.mogrify(where, tuple(rules[mode][1]))
+                        where = self.env.cr.mogrify(
+                            where, tuple(rules[mode][1]))
 
                         check_rules.append((0, 0, {
                             'ir_model_id': ir_model.id,
@@ -118,40 +100,49 @@ class check_user_access(osv.TransientModel):
         return True
 
 
-class check_user_access_line(osv.TransientModel):
+class CheckUserAccessLine(models.TransientModel):
     _name = 'check.user.right'
     _description = 'User Applied Right'
     _rec_name = 'ir_model_id'
 
-    _columns = {
-        'check_access_id': fields.many2one('check.user.access', string='User access check'),
-        'ir_model_id': fields.many2one('ir.model', string='Model', help='Model affected by this access right'),
-        'model': fields.related('ir_model_id', 'model', type='char', size=64, string='Model Name', help='Name of the model affected by this access right'),
-        'perm_read': fields.boolean(string='Read Access', help='Read permission'),
-        'perm_create': fields.boolean(string='Create Access', help='Creation permission'),
-        'perm_write': fields.boolean(string='Write Access', help='Write permission'),
-        'perm_unlink': fields.boolean(string='Unlink Access', help='Deletion permission'),
-    }
+    check_access_id = fields.Many2one(
+        comodel_name='check.user.access', string='User access check')
+    ir_model_id = fields.Many2one(
+        comodel_name='ir.model', string='Model',
+        help='Model affected by this access right.')
+    model = fields.Char(
+        related='ir_model_id.model', string='Model Name',
+        help='Name of the model affected by this access right.')
+    perm_read = fields.Boolean(
+        string='Read Access', help='Read permission.')
+    perm_create = fields.Boolean(
+        string='Create Access', help='Creation permission.')
+    perm_write = fields.Boolean(
+        string='Write Access', help='Write permission.')
+    perm_unlink = fields.Boolean(
+        string='Unlink Access', help='Deletion permission.')
 
-    _defaults = {
-        'perm_read': False,
-        'perm_create': False,
-        'perm_write': False,
-        'perm_unlink': False,
-    }
 
-
-class check_user_rule(osv.TransientModel):
+class CheckUserRule(models.TransientModel):
     _name = 'check.user.rule'
     _description = 'User Applied Rule'
     _rec_name = 'ir_model_id'
 
-    _columns = {
-        'check_access_id': fields.many2one('check.user.access', string='User access check'),
-        'ir_model_id': fields.many2one('ir.model', string='Model Name', help='Model affected by this rule'),
-        'model': fields.related('ir_model_id', 'model', type='char', size=64, string='Model', help='Name of the model affected by this rule'),
-        'rule': fields.text(string='Rules', help='Rule applied for this model'),
-        'mode': fields.selection([('read', 'Read'), ('create', 'Create'), ('write', 'Write'), ('unlink', 'Unlink')], 'Mode', help='Mode on which this rule is applied'),
-    }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    check_access_id = fields.Many2one(
+        comodel_name='check.user.access', string='User access check')
+    ir_model_id = fields.Many2one(
+        comodel_name='ir.model', string='Model Name',
+        help='Model affected by this rule.')
+    model = fields.Char(
+        related='ir_model_id.model', string='Model',
+        help='Name of the model affected by this rule.')
+    rule = fields.Text(
+        string='Rules', help='Rule applied for this model.')
+    mode = fields.Selection(
+        selection=[
+            ('read', 'Read'),
+            ('create', 'Create'),
+            ('write', 'Write'),
+            ('unlink', 'Unlink'),
+        ], string='Mode',
+        help='Mode on which this rule is applied')
